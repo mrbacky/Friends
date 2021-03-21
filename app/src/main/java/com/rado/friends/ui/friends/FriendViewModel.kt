45feaf2e0d8d6a4354e3a4f4e0ed0b281ff913a5
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2021-03-21 02:57:43 +0100

    swipe to delete friend
    event channel for collecting events from viewmodel in fragment

--- app/src/main/java/com/rado/friends/ui/friends/FriendViewModel.kt
+++ app/src/main/java/com/rado/friends/ui/friends/FriendViewModel.kt
@@ -8,30 +8,49 @@ import com.rado.friends.data.Friend
 import com.rado.friends.data.FriendDAO
 import dagger.assisted.Assisted
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FriendViewModel @Inject constructor(
     private val friendDao: FriendDAO,
+    @Assisted private val state: SavedStateHandle
 ) : ViewModel() {
-
 
     val friends = friendDao.getFriends().asLiveData()
 
+    private val friendEventChannel = Channel<FriendEvent>()
+    val friendEvent = friendEventChannel.receiveAsFlow()
+
+
+    fun onFriendSelected(friend: Friend) = viewModelScope.launch {
+        friendEventChannel.send(FriendEvent.NavigateToEditFriendScreen(friend))
+    }
+
+    fun onAddNewFriendClick() = viewModelScope.launch {
+        friendEventChannel.send(FriendEvent.NavigateToAddFriendScreen)
+    }
+
+    fun onFriendSwiped(friend: Friend) = viewModelScope.launch {
+        friendDao.delete(friend)
+        friendEventChannel.send(FriendEvent.ShowUndoDeleteFriendMessage(friend))
+    }
+
+    fun onUndoDeleteClick(friend: Friend) = viewModelScope.launch {
+        friendDao.insert(friend)
+    }
 
     sealed class FriendEvent {
 
+        data class ShowUndoDeleteFriendMessage(val friend: Friend) : FriendEvent()
+
         object NavigateToAddFriendScreen : FriendEvent()
+
         data class NavigateToEditFriendScreen(val friend: Friend) : FriendEvent()
 
     }
 
 
-    fun onFriendSelected(friend: Friend) {
-
-    }
-
-    fun onAddNewFriendClick() = viewModelScope.launch {
-    }
 }
--- app/src/main/java/com/rado/friends/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/rado/friends/ui/friends/FriendsFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.rado.friends.R
 import com.rado.friends.data.Friend
 import com.rado.friends.databinding.FragmentFriendsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class FriendsFragment : Fragment(R.layout.fragment_friends), FriendAdapter.OnItemClickListener {
@@ -31,6 +36,29 @@ class FriendsFragment : Fragment(R.layout.fragment_friends), FriendAdapter.OnIte
 
             }
 
+
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+                0,
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            ) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false;
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val friend = friendAdapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onFriendSwiped(friend)
+
+
+                }
+
+
+            }).attachToRecyclerView(rvFriends)
+
             fabAddFriend.setOnClickListener {
                 viewModel.onAddNewFriendClick()
             }
@@ -38,6 +66,24 @@ class FriendsFragment : Fragment(R.layout.fragment_friends), FriendAdapter.OnIte
 
         viewModel.friends.observe(viewLifecycleOwner, friendAdapter::submitList)
 
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.friendEvent.collect { event ->
+                when (event) {
+                    is FriendViewModel.FriendEvent.ShowUndoDeleteFriendMessage -> {
+                        Snackbar.make(
+                            requireView(),
+                            "Friend deleted",
+                            Snackbar.LENGTH_LONG
+                        ).setAction("UNDO") {
+                            viewModel.onUndoDeleteClick(event.friend)
+                        }.show()
+                    }
+
+
+                }
+
+            }
+        }
 
     }
 
